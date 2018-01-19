906cc00d80ef514475b663d32a3e2555c34a4aa1
Lee Barber <lfb11@alumni.psu.edu>
2018-01-19 11:06:10 -0500

    fixed js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-function palindrome (str) {
-	return true;
+function palindrome (x){
+	var y = x.split('').reverse().join('');
+	if (x.toLowerCase()===y.toLowerCase()){
+		console.log( x  + ' is a palindrome.');
+	} else {
+		console.log(x  + ' is not a palindrome.');
+	}
 }
-
-palindrome("kayak")
